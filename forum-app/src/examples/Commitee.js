b5c5ba5deaff78f688e7c69db0107e47165e910d
clairelist <claire.list2501@gmail.com>
2023-03-28 19:49:31 -0600

    are you happy

--- forum-app/src/examples/Commitee.js
+++ forum-app/src/examples/Commitee.js
@@ -1,1 +1,17 @@
 //I DISPLAY all topics.
+
+import {useState, useEffect, useRef} from 'react';
+
+function Commitee () {
+    const [topics, setTopics] = useState([]);
+    //REMEMBER we must use useRef with useEffect!
+    return (
+        <div className='Committee'>
+            <h1>I am the GENCOM - general discussion etc etc.</h1>
+            {topics.map(()=>{ //...do something here, lol.
+                
+                <div className='topics'>{topics}</div>
+            })}
+        </div>
+    )
+}
