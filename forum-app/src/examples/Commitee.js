2361def02fa612565df4766d16313ccac932e557
clairelist <claire.list2501@gmail.com>
2023-03-28 20:09:56 -0600

    some skeletons are coming out tha closet

--- forum-app/src/examples/Commitee.js
+++ forum-app/src/examples/Commitee.js
@@ -1,16 +1,29 @@
 //I DISPLAY all topics.
 
+import {Topics} from './Topics';
 import {useState, useEffect, useRef} from 'react';
 
 function Commitee () {
     const [topics, setTopics] = useState([]);
     //REMEMBER we must use useRef with useEffect!
+
+    const fetch = useRef(()=>{}); //I NEED to be initialized as an anon function which yields an empty object! Because of the way we will be using fetch, below...
+
+    fetch.current = ()=> {
+        //api call here!
+        //... in example case, we need only set the topics into state.
+        setTopics(Topics);
+    }
+
+    useEffect(()=>{
+        fetch.current();
+    }, [])
     return (
         <div className='Committee'>
             <h1>I am the GENCOM - general discussion etc etc.</h1>
             {topics.map(()=>{ //...do something here, lol.
                 
-                <div className='topics'>{topics}</div>
+                <div className='topics'>{topics.title}</div>
             })}
         </div>
     )
--- forum-app/src/examples/Topics.js
+++ forum-app/src/examples/Topics.js
@@ -4,7 +4,7 @@
 //once you click on a topic, THEN the replies for that topic are displayed!
 
 
-const Topics = {
+export const Topics = {
     topic_one: {
     committee_name: 'GENCOM',
     id: 1,
