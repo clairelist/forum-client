2908a63a28c46049e717251ce0f7e4fdb6eaf7ea
clairelist <claire.list2501@gmail.com>
2023-03-29 08:44:51 -0600

    yeah why should dates be human readable brilliant job

--- forum-app/src/examples/Commitee.js
+++ forum-app/src/examples/Commitee.js
@@ -1,10 +1,12 @@
 //I DISPLAY all topics.
 
+//TODO: CREATE A DATE FORMATTER GOD DAMN IT!!!
+
 import {Topics} from './Topics';
 import {useState, useEffect, useRef} from 'react';
 
 function Commitee () {
-    const [topics, setTopics] = useState([]);
+    const [topics, setTopics] = useState(Topics); //I can TECHNICALLY be set to an empty array, but react will complain that Topics, above, is an unused variable. But if you comment that import statement out, you cannot display topics, below. WHAT ??? Is my computer HAUNTED???
     //REMEMBER we must use useRef with useEffect!
 
     const fetch = useRef(()=>{}); //I NEED to be initialized as an anon function which yields an empty object! Because of the way we will be using fetch, below...
@@ -13,7 +15,7 @@ function Commitee () {
         //api call here!
         //... in example case, we need only set the topics into state.
         setTopics([...topics ]);
-        console.log(topics[0].title);
+       
     }
 
     useEffect(()=>{
@@ -23,9 +25,13 @@ function Commitee () {
         <div className='Committee'>
             <h1>I am the GENCOM - general discussion etc etc.</h1>
             {topics.map((topic)=>{ 
-                <div className='topics-wrapper' key={topic.id}>
+                return <div className='topics-wrapper' key={topic.id}>
                 
                 <h2 className='topic-title'>{topic.title}</h2>
+                <p>Date created: {topic.date_created}</p>
+                <p>Created by: {topic.created_by}</p>
+                <p>Last updated: {topic.last_reply}</p>
+                <p>Number of replies: {topic.num_replies}</p>
                 </div>
             })}
         </div>
